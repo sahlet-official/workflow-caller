576b817dff8bea2fc2790a6abe84d73e8f587c7f
Sahlet <sahlet.official@gmail.com>
2025-05-02 12:06:42 +0200

    update

--- scripts/call-workflow-and-get-result.py
+++ scripts/call-workflow-and-get-result.py
@@ -6,8 +6,12 @@ import requests
 import time
 import json
 import uuid
+import zipfile
+import shutil
 from datetime import datetime, timezone
 from urllib.parse import quote
+
+# --------------------------------------------------------
 
 def add_run_unique_id(input_json):
     try:
@@ -130,7 +134,7 @@ def wait_for_run_complete_and_get(owner, repo, run_id, token):
         return None
 
 # https://docs.github.com/en/rest/actions/artifacts?apiVersion=2022-11-28#list-workflow-run-artifacts
-def get_run_artifacts(owner, repo, run_id, token):
+def get_run_artifacts_info(owner, repo, run_id, token):
     url = f"https://api.github.com/repos/{owner}/{repo}/actions/runs/{run_id}/artifacts"
 
     headers = {
@@ -145,6 +149,59 @@ def get_run_artifacts(owner, repo, run_id, token):
 
         artifacts = response.json().get("artifacts", [])
         return artifacts
+    except Exception as err:
+        print(f"An error occurred: {err}", file=sys.stderr)
+        return None
+
+# https://docs.github.com/en/rest/actions/artifacts?apiVersion=2022-11-28#download-an-artifact
+def get_artifact_download_url(owner, repo, artifact_id, archive_format, token):
+    url = f"https://api.github.com/repos/{owner}/{repo}/actions/artifacts/{artifact_id}/{archive_format}"
+
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {token}",
+        "X-GitHub-Api-Version": "2022-11-28"
+    }
+
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+
+        download_url = response.json().get("url", "")
+        return download_url
+    except Exception as err:
+        print(f"An error occurred: {err}", file=sys.stderr)
+        return None
+    
+def download_and_extract_artifact(artifact_download_url, artifact_file_name):
+    try:
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        tmp_dir = os.path.join(base_dir, "artifact_tmp")
+        zip_path = os.path.join(tmp_dir, "result.zip")
+        extract_dir = os.path.join(tmp_dir, "artifact")
+
+        os.makedirs(tmp_dir, exist_ok=True)
+
+        response = requests.get(artifact_download_url)
+        response.raise_for_status()
+
+        with open(zip_path, "wb") as f:
+            f.write(response.content)
+
+        with zipfile.ZipFile(zip_path, "r") as zip_ref:
+            zip_ref.extractall(extract_dir)
+
+        result_json_path = os.path.join(extract_dir, artifact_file_name)
+
+        if not os.path.isfile(result_json_path):
+            raise FileNotFoundError(f"file {artifact_file_name} is not found: {result_json_path}")
+
+        with open(result_json_path, "r", encoding="utf-8") as f:
+            content = f.read()
+
+        shutil.rmtree(tmp_dir)
+
+        return content
     except Exception as err:
         print(f"An error occurred: {err}", file=sys.stderr)
         return None
@@ -212,51 +269,46 @@ print("✅ Run succeeded", file=sys.stderr)
 
 # --------------------------------------------------------
 
-artifacts = get_run_artifacts(OWNER_NAME, REPO_NAME,  run.get("id", ""), INSTALLATION_TOKEN)
+artifacts = get_run_artifacts_info(OWNER_NAME, REPO_NAME, run.get("id", ""), INSTALLATION_TOKEN)
 
 if not artifacts:
-    print("❌ Cant get run artifacts", file=sys.stderr)
+    print("❌ Cant get run artifacts info", file=sys.stderr)
     sys.exit(1)
 
-print("✅ Got run artifacts", file=sys.stderr)
+print("✅ Got run artifacts info", file=sys.stderr)
 
 # --------------------------------------------------------
 
 result_artifact = None
+artifact_name = "result"
 
 for artifact in artifacts:
     name = artifact.get("name", "")
 
-    if name == "result":
+    if name == artifact_name:
         result_artifact = artifact
 
 if not result_artifact:
-    print("￣\_(ツ)_/￣ There is no 'result' artifact", file=sys.stderr)
+    print(f"￣\_(ツ)_/￣ There is no '{artifact_name}' artifact", file=sys.stderr)
     sys.exit(0)
 
-    # const downloadResponse = await octokit.actions.downloadArtifact({
-    #     owner,
-    #     repo,
-    #     artifact_id: targetArtifact.id,
-    #     archive_format: "zip",
-    # });
-    
-    # const downloadUrl = (downloadResponse as { url: string }).url;
+artifact_download_url = get_artifact_download_url(OWNER_NAME, REPO_NAME, result_artifact.get("id", ""), "zip", INSTALLATION_TOKEN)
 
-    # const zipPath = path.join(__dirname, "result.zip");
-    # const res = await fetch(downloadUrl);
-    # const arrayBuffer = await res.arrayBuffer();
-    # await fs.promises.writeFile(zipPath, Buffer.from(arrayBuffer));
+if not artifact_download_url:
+    print("❌ Cant get artifact_download_url", file=sys.stderr)
+    sys.exit(1)
 
-    # await fs
-    #     .createReadStream(zipPath)
-    #     .pipe(unzipper.Extract({ path: path.join(__dirname, "artifact") }))
-    #     .promise();
+# --------------------------------------------------------
 
-    # const resultJsonPath = path.join(__dirname, "artifact", "result.json");
-    # const content = fs.readFileSync(resultJsonPath, "utf8");
-    # const parsed = JSON.parse(content);
+result_file_name = "result.json"
+run_result = download_and_extract_artifact(artifact_download_url, result_file_name)
+
+if not run_result:
+    print(f"￣\_(ツ)_/￣ There is no '{result_file_name}' in '{artifact_name}' artifact", file=sys.stderr)
+    sys.exit(0)
 
 print("✅ Printing workflow result", file=sys.stderr)
 
-sys.stdout.write(installation_token)
+sys.stdout.write(run_result)
+
+# --------------------------------------------------------
